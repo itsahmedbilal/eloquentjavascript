504251acc0e8cc021269c20d4035890101bf7224
ahmedbilal1994 <ahmedbillalimtiazghumman@gmail.com>
2024-09-24 00:41:14 +0500

    chapter 4 problems are solved

---
+++ chapter-4/deep-comparison.js
@@ -0,0 +1,35 @@
+function deepEqual(object1, object2) {
+  if (object1 === object2) {
+    return true;
+  } else {
+    if (
+      object1 == null ||
+      object2 == null ||
+      typeof object1 !== "object" ||
+      typeof object2 !== "object"
+    ) {
+      return false;
+    } else {
+      const keysOfObject1 = Object.keys(object1);
+      const keysOfObject2 = Object.keys(object2);
+      if (keysOfObject1.length !== keysOfObject2.length) return false;
+      for (const key of keysOfObject1) {
+        if (
+          !keysOfObject2.includes(key) ||
+          !deepEqual(object1[key], object2[key])
+        ) {
+          return false;
+        }
+        return true;
+      }
+    }
+  }
+}
+
+let obj = { here: { is: "an" }, object: 2 };
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, { here: 1, object: 2 }));
+// → false
+console.log(deepEqual(obj, { here: { is: "an" }, object: 2 }));
+// → true
